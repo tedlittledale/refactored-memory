8cefe96473d7423731818c639a4999bf0b63640c
Ted Littledale <ted.littledale@gmail.com>
2021-04-06 13:03:03 +0100

    Favicon and pwa

--- next.config.js
+++ next.config.js
@@ -1,5 +1,10 @@
-module.exports = {
+const withPWA = require("next-pwa");
+
+module.exports = withPWA({
   images: {
     domains: ["webdev-exercise.netlify.com"]
+  },
+  pwa: {
+    dest: "public"
   }
-};
+});
--- pages/index.js
+++ pages/index.js
@@ -43,6 +43,7 @@ const Home = () => {
 
         <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
         <meta name="theme-color" content="#ffffff" />
+        <link rel="manifest" href="/manifest.json" />
       </Head>
       <Layout>
         <NavBar>
